baba2fc92cfa0fdfcf2a083939dc30b525c9e444
Aman722001 <asingh18@mail.pima.edu>
2024-09-21 15:59:53 -0700

    Add files via upload
    Lab4

---
+++ CIS129_AmanpreetSingh_Lab4.py
@@ -0,0 +1,43 @@
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+prompt = "Enter the monthly sales: "  # prompt will be a string literal
+
+# This code gets the monthly sales
+monthlySales = float(input(prompt))
+
+# This code determines the store bonus
+if monthlySales >= 110000:
+    storeAmount = 6000
+elif monthlySales >= 100000:
+    storeAmount = 5000
+elif monthlySales >= 90000:
+    storeAmount = 4000
+elif monthlySales >= 80000:
+    storeAmount = 3000
+else:
+    storeAmount = 0
+
+# This code gets the percent of increase in sales
+salesIncrease = float(input("Enter the percent of sales increase (in decimal format, e.g., 5 for 5%): "))
+salesIncrease = salesIncrease / 100  # Convert to decimal format
+
+# This code determines the employee bonus
+if salesIncrease >= 0.05:
+    empAmount = 75
+elif salesIncrease >= 0.04:
+    empAmount = 50
+elif salesIncrease >= 0.03:
+    empAmount = 40
+else:
+    empAmount = 0
+
+# This code prints the bonus information
+print("The store bonus amount is $", storeAmount)
+print("The employee bonus amount is $", empAmount)
+
+# Check if the highest bonus amounts were achieved
+if storeAmount == 6000 and empAmount == 75:
+    print('Congrats! You have reached the highest bonus amounts possible!')
